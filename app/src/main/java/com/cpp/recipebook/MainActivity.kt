76747091c40217fe1c1cc3ad164f983f73900720
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-22 16:50:53 -0800

    Rename package

--- app/src/main/java/com/cpp/recipebook/MainActivity.kt
+++ app/src/main/java/com/cpp/recipebook/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.cpp.recipebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/cpp/recipebook/ExampleUnitTest.kt
+++ app/src/test/java/com/cpp/recipebook/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.cpp.recipebook
 
 import org.junit.Test
 
