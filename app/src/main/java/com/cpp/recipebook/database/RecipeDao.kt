d7b432526b13872b7e5cff52b68997d3ffb5e8b1
ikenton <imkenton@cpp.edu>
2023-11-22 20:53:57 -0800

    created recipe repo, updated dao

--- app/src/main/java/com/cpp/recipebook/database/RecipeDao.kt
+++ app/src/main/java/com/cpp/recipebook/database/RecipeDao.kt
@@ -1,10 +1,12 @@
 package com.cpp.recipebook.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.cpp.recipebook.Recipe
+import java.util.UUID
 
 @Dao
 interface RecipeDao {
@@ -12,13 +14,16 @@ interface RecipeDao {
     @Query("SELECT * FROM recipe")
     fun getRecipes(): List<Recipe>
 
+    //get single recipe
+    @Query("SELECT * FROM recipe WHERE id=(:id)")
+    suspend fun getRecipe(id: UUID):Recipe
     // add a recipe
     @Insert
     fun addRecipe(recipe: Recipe)
 
     // delete a recipe
-    @Query("DELETE FROM recipe WHERE id=(:id)")
-    fun deleteRecipe(id: Int)
+    @Delete
+    fun deleteRecipe(vararg recipe: Recipe)
 
     // update a recipe
     @Update
