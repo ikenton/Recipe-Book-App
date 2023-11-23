381f278215f67d98c452caa3182625499a671730
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-22 17:11:08 -0800

    Added room annotations

--- app/src/main/java/com/cpp/recipebook/Recipe.kt
+++ app/src/main/java/com/cpp/recipebook/Recipe.kt
@@ -1,6 +1,11 @@
 package com.cpp.recipebook
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Recipe (
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val cuisine: String,
     val ingredients: MutableList<String>,
