c33e4aef533e34908a4cf4e7388393cfe43a8be2
ikenton <imkenton@cpp.edu>
2023-11-22 20:12:56 -0800

    functioning columns

--- app/src/main/java/com/cpp/recipebook/MainActivity.kt
+++ app/src/main/java/com/cpp/recipebook/MainActivity.kt
@@ -1,28 +1,76 @@
 package com.cpp.recipebook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            DisplayColumns()
+        }
     }
+}
+val listOfNames = listOf("American Pie", "Tacos")
+val listOfCuisines = listOf("American", "Mexican")
+val listOfImages = listOf(R.drawable.hamburger, R.drawable.ic_launcher_background)
+@Composable
+fun DisplayColumns(){
+    Row(
 
+        modifier = Modifier.fillMaxWidth()
+        //verticalAlignment = Alignment.Top
+    ) {
+        LazyColumn {
+            items(listOfImages) { image ->
+                Column(
+                    modifier = Modifier.weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource(id = image),
+                        contentDescription = "Image",
+
+                    )
+                    Text(text = "American Burger")
+                    Text(text = "Cuisine")
+                }
+            }
+        }
+
+        LazyColumn {
+            items(listOfImages) { image ->
+                Column(
+                    modifier = Modifier.weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource(id = image),
+                        contentDescription = "Image",
+
+                    )
+                    Text(text = "Pie")
+                    Text(text = "Cuisine")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ComposablePreview(){
+    DisplayColumns()
 }
