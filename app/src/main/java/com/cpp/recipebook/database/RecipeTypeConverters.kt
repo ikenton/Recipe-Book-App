be9e6a5d5a5b2647e7e2a5a6f86c3c347de48252
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-22 17:25:53 -0800

    TypeConverters

--- app/src/main/java/com/cpp/recipebook/database/RecipeTypeConverters.kt
+++ app/src/main/java/com/cpp/recipebook/database/RecipeTypeConverters.kt
@@ -1,7 +1,24 @@
 package com.cpp.recipebook.database
 
 import androidx.room.TypeConverter
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
 
 class RecipeTypeConverters {
+    private val moshi = Moshi.Builder().build()
+    private val listType = com.squareup.moshi.Types.newParameterizedType(
+        MutableList::class.java,
+        String::class.java
+    )
+    private val jsonAdapter: JsonAdapter<MutableList<String>> = moshi.adapter(listType)
 
+    @TypeConverter
+    fun fromString(value: String): MutableList<String>? {
+        return jsonAdapter.fromJson(value)
+    }
+
+    @TypeConverter
+    fun fromList(list: MutableList<String>): String {
+        return jsonAdapter.toJson(list)
+    }
 }
