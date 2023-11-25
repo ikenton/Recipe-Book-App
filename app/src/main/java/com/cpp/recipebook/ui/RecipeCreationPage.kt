8885b3c713196848d15ebb75ba4538630ae3830c
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-25 14:06:58 -0800

    Added material elements

--- app/src/main/java/com/cpp/recipebook/ui/RecipeCreationPage.kt
+++ app/src/main/java/com/cpp/recipebook/ui/RecipeCreationPage.kt
@@ -1,16 +1,28 @@
 package com.cpp.recipebook.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeCreationPage() {
     Column {
-        TextField(value = "", onValueChange = {}, label = { Text("Recipe Name") })
-
+        OutlinedTextField(value = "", onValueChange = {}, label = { Text("Recipe Name") }, modifier = androidx.compose.ui.Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(value = "", onValueChange = {}, label = { Text("Cuisine") }, modifier = androidx.compose.ui.Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedButton(onClick = { /* Do something */ }, modifier = androidx.compose.ui.Modifier.fillMaxWidth()) {
+            Text("Add Image")
+        }
     }
 }
 
