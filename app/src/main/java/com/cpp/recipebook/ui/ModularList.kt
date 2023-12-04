bfe5adae424e7640d9e0ec5cd85570d9111b714e
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-03 17:56:28 -0800

    Make adding ingredients actually possible

--- app/src/main/java/com/cpp/recipebook/ui/ModularList.kt
+++ app/src/main/java/com/cpp/recipebook/ui/ModularList.kt
@@ -5,23 +5,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ModularList(items: MutableList<String>, onItemAdded: (String) -> Unit) {
+    var text by remember { mutableStateOf("") }
     Column {
         items.forEach { item ->
             ListItem(headlineContent = { Text(item) })
         }
-        OutlinedTextField(value = "", onValueChange = { newItem ->
-            if (newItem.isNotEmpty()) {
-                onItemAdded(newItem)
-            }
+        OutlinedTextField(value = text, onValueChange = { newItem ->
+            text = newItem
         },
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (text.isNotEmpty()) {
+                        onItemAdded(text)
+                        text = ""
+                    }
+                }
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
             label = { Text("Add Item") },
             modifier = Modifier.fillMaxWidth()
         )
