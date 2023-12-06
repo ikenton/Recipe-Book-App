8d4870225a5b3c51cd20aab7a03ea5c22c9d6b4f
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-06 11:51:13 -0800

    Use recipe instead of recipeId

--- app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListEvent.kt
+++ app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListEvent.kt
@@ -1,8 +1,9 @@
 package com.cpp.recipebook.ui.recipe_list
 
+import com.cpp.recipebook.database.Recipe
 import java.util.UUID
 
 sealed class RecipeListEvent {
-    data class onRecipeClick(val recipeId: Int): RecipeListEvent()
+    data class onRecipeClick(val recipe: Recipe): RecipeListEvent()
     object onAddRecipeClick: RecipeListEvent()
 }
