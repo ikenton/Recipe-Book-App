f98554b802c83cf8543adce07a01058c9af60229
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-05 22:45:21 -0800

    Added hilt nav compose

--- app/src/main/java/com/cpp/recipebook/ui/create_update_recipe/CreateUpdateRecipeScreen.kt
+++ app/src/main/java/com/cpp/recipebook/ui/create_update_recipe/CreateUpdateRecipeScreen.kt
@@ -1,2 +1,12 @@
 package com.cpp.recipebook.ui.create_update_recipe
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CreateUpdateRecipeScreen(
+    viewModel: RecipeCreationViewModel = hiltViewModel(), // 1:19
+    // might need popback nav controller thing
+) {
+
+}
