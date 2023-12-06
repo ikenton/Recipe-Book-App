5b0227059827878b5dc2131dc98246ca53313690
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-06 01:33:08 -0800

    Update event to require uuid

--- app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListCard.kt
+++ app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListCard.kt
@@ -1,2 +1,21 @@
 package com.cpp.recipebook.ui.recipe_list
 
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.cpp.recipebook.Recipe
+
+@Composable
+fun RecipeListCard(
+    recipe: Recipe,
+    onEvent: (RecipeListEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(
+        onClick = { onEvent(RecipeListEvent.onRecipeClick(recipe.id)) },
+        modifier = modifier.width(200.dp)
+    ) {
+
+    }
+}
--- app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListEvent.kt
+++ app/src/main/java/com/cpp/recipebook/ui/recipe_list/RecipeListEvent.kt
@@ -1,6 +1,8 @@
 package com.cpp.recipebook.ui.recipe_list
 
+import java.util.UUID
+
 sealed class RecipeListEvent {
-    data class onRecipeClick(val recipeId: String): RecipeListEvent()
+    data class onRecipeClick(val recipeId: UUID): RecipeListEvent()
     object onAddRecipeClick: RecipeListEvent()
 }
