04fbf7d7ff30818c7e91a30270d7109809c14e01
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-05 21:27:15 -0800

    repo impl

--- app/src/main/java/com/cpp/recipebook/database/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/cpp/recipebook/database/RecipeRepositoryImpl.kt
@@ -1,4 +1,32 @@
 package com.cpp.recipebook.database
 
-class RecipeRepositoryImpl {
+import com.cpp.recipebook.Recipe
+import java.util.UUID
+
+class RecipeRepositoryImpl(
+    private val recipeDao: RecipeDao
+): RecipeRepository {
+    override fun getRecipes(): List<Recipe> {
+        return recipeDao.getRecipes()
+    }
+
+    override suspend fun getRecipe(id: UUID): Recipe {
+        return recipeDao.getRecipe(id)
+    }
+
+    override suspend fun getName(id: UUID): String {
+        return recipeDao.getName(id)
+    }
+
+    override suspend fun getCuisine(id: UUID): String {
+        return recipeDao.getCuisine(id)
+    }
+
+    override fun addRecipe(recipe: Recipe) {
+        recipeDao.addRecipe(recipe)
+    }
+
+    override fun deleteRecipe(vararg recipe: Recipe) {
+        recipeDao.deleteRecipe(*recipe)
+    }
 }
