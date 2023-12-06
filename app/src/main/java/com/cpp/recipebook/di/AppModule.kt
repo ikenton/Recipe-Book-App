82552501181c1738493dd6f4f58ae5c39e8628b6
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-05 21:46:43 -0800

    Defining repos for dependency injection

--- app/src/main/java/com/cpp/recipebook/di/AppModule.kt
+++ app/src/main/java/com/cpp/recipebook/di/AppModule.kt
@@ -1,4 +1,32 @@
 package com.cpp.recipebook.di
 
+import android.app.Application
+import androidx.room.Room
+import com.cpp.recipebook.database.RecipeDatabase
+import com.cpp.recipebook.database.RecipeRepository
+import com.cpp.recipebook.database.RecipeRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideRecipeDatabase(app: Application): RecipeDatabase {
+        return Room.databaseBuilder(
+            app,
+            RecipeDatabase::class.java,
+            "recipe_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecipeRepository(db: RecipeDatabase): RecipeRepository {
+        return RecipeRepositoryImpl(db.recipeDao()) // check on this later
+    }
 }
