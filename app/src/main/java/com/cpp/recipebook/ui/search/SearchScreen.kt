2f16da9cf96a7e679a1c22d4f2a76e07fce66735
mikumino <36429067+mikumino@users.noreply.github.com>
2023-12-06 22:21:06 -0800

    Focus on nav

--- app/src/main/java/com/cpp/recipebook/ui/search/SearchScreen.kt
+++ app/src/main/java/com/cpp/recipebook/ui/search/SearchScreen.kt
@@ -17,7 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.cpp.recipebook.util.UiEvent
@@ -28,7 +31,10 @@ fun SearchScreen (
     onNavigate: (UiEvent.Navigate) -> Unit,
     onPopBackStack: () -> Unit
 ) {
+    val focusRequester = remember { FocusRequester() }
+
     LaunchedEffect(key1 = true) {
+        focusRequester.requestFocus()
         viewModel.uiEvent.collect { uiEvent ->
             when (uiEvent) {
                 is UiEvent.ShowSnackbar -> {
@@ -79,6 +85,7 @@ fun SearchScreen (
             modifier = Modifier
                 .padding(values)
                 .fillMaxWidth()
+                .focusRequester(focusRequester)
         ) {
             // TODO: Search results
         }
