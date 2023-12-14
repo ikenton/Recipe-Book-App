1d6c59e62c59ea9b0fa6d125d0efd26127ab4d5d
ikenton <isabellakenton@gmail.com>
2023-12-13 17:25:00 -0800

    recipepage scrolls in horizontal landscape

--- app/src/main/java/com/cpp/recipebook/ui/recipe_page/RecipePage.kt
+++ app/src/main/java/com/cpp/recipebook/ui/recipe_page/RecipePage.kt
@@ -1,14 +1,17 @@
 package com.cpp.recipebook.ui.recipe_page
 
+import android.content.res.Configuration
 import android.net.Uri
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -22,13 +25,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
-import com.cpp.recipebook.database.Recipe
 
 @Composable
 fun RecipePage(
@@ -41,6 +44,10 @@ fun RecipePage(
     //tab info
     var tabIndex by remember {mutableStateOf(0)}
     val tabs = listOf("Ingredients", "Directions", "Notes")
+    var isLandscape by remember{ mutableStateOf(false)}
+    val configuration = LocalConfiguration.current
+    isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val listState = rememberLazyListState()
     Scaffold(
         modifier = Modifier
             .fillMaxSize(),
@@ -49,17 +56,70 @@ fun RecipePage(
             FloatingActionButton(onClick = { /*viewModel.onEvent(RecipePageEvent.onEditRecipeClick)*/
             }) {
                 Icon(
-                    imageVector = Icons.Default.Add,
+                    imageVector = Icons.Default.Edit,
                     contentDescription = "Edit Recipe"
                 )
             }
         }
-    ) { values ->
-        Box(
-            modifier = Modifier
-                .padding(values)
-        ){
-            Column(){
+    )
+    { values ->
+        if(isLandscape){
+            LazyColumn {
+                item {
+                    AsyncImage(
+                        model = Uri.parse(viewModel.image),
+                        contentDescription = viewModel.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(180.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                item{
+                    Text(
+                        text = viewModel.name,
+                        style = TextStyle(
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier
+                            .padding(start = 10.dp, top = 10.dp)
+                    )
+                }
+                item{
+                    Text(
+                        text = viewModel.cuisine,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                    )
+                }
+                item{
+                    TabRow(selectedTabIndex = tabIndex) {
+                        tabs.forEachIndexed { index, title ->
+                            Tab(
+                                text = { Text(title) },
+                                selected = tabIndex == index,
+                                onClick = { tabIndex = index },
+                            )
+                        }
+                    }
+                }
+                item{
+                    when(tabIndex){
+                        0 -> Ingredients(viewModel, isLandscape)
+                        1 -> Directions(viewModel, isLandscape)
+                        2 -> Notes(viewModel, isLandscape)
+                    }
+                }
+            }
+        }else{
+            Column(
+
+                modifier = Modifier
+                    .padding(values)
+            ){
+
                 AsyncImage(
                     model = Uri.parse(viewModel.image),
                     contentDescription = viewModel.name,
@@ -68,29 +128,133 @@ fun RecipePage(
                         .height(180.dp),
                     contentScale = ContentScale.Crop
                 )
+
                 Text(
                     text = viewModel.name,
                     style = TextStyle(
-                        fontSize = 30.sp,
+                        fontSize = 40.sp,
                         fontWeight = FontWeight.Bold
-                    )
+                    ),
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 10.dp)
                 )
+
                 Text(
                     text = viewModel.cuisine,
-                    fontSize = 15.sp
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(start = 10.dp)
                 )
-                TabRow(selectedTabIndex = tabIndex){
-                    tabs.forEachIndexed{index, title ->
+                TabRow(selectedTabIndex = tabIndex) {
+                    tabs.forEachIndexed { index, title ->
                         Tab(
-                            text = {Text(title)},
+                            text = { Text(title) },
                             selected = tabIndex == index,
-                            onClick = {tabIndex = index},
+                            onClick = { tabIndex = index },
                         )
                     }
                 }
-
+                when(tabIndex){
+                    0 -> Ingredients(viewModel, isLandscape)
+                    1 -> Directions(viewModel, isLandscape)
+                    2 -> Notes(viewModel, isLandscape)
+                }
             }
         }
     }
 }
+
+@Composable
+fun Ingredients(viewModel: RecipePageViewModel, isLandscape: Boolean){
+    val listState = rememberLazyListState()
+    if(isLandscape){
+        LazyRow(
+            state = listState,
+            modifier = Modifier
+                .padding(20.dp)
+        ){
+            item{
+                Text(
+                    text = viewModel.ingredients,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }else{
+        LazyColumn(state = listState,
+            modifier = Modifier
+                .padding(20.dp)){
+            item{
+
+                Text(
+                    text = viewModel.ingredients,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Directions(viewModel: RecipePageViewModel, isLandscape: Boolean){
+    val listState = rememberLazyListState()
+    if(isLandscape){
+        LazyRow(
+            state = listState,
+            modifier = Modifier
+                .padding(20.dp)
+        ){
+            item{
+                Text(
+                    text = viewModel.directions,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }else{
+        LazyColumn(state = listState,
+            modifier = Modifier
+                .padding(20.dp)){
+            item{
+
+                Text(
+                    text = viewModel.directions,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Notes(viewModel: RecipePageViewModel, isLandscape: Boolean){
+    val listState = rememberLazyListState()
+    if(isLandscape){
+        LazyRow(
+            state = listState,
+            modifier = Modifier
+                .padding(20.dp)
+        ){
+            item{
+                Text(
+                    text = viewModel.notes,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }else{
+        LazyColumn(state = listState,
+            modifier = Modifier
+                .padding(20.dp)){
+            item{
+
+                Text(
+                    text = viewModel.notes,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }
+}
+
 
